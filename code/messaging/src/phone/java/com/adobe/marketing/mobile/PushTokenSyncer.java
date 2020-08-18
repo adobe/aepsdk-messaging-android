f97e0dfa5943a0d2d17734e12dd179bfc9efffe1
ravinsingh jain <ravjain@adobe.com>
2020-08-17 17:21:37 -0700

    Adding comments about which schema and datasetid

--- code/messaging/src/phone/java/com/adobe/marketing/mobile/PushTokenSyncer.java
+++ code/messaging/src/phone/java/com/adobe/marketing/mobile/PushTokenSyncer.java
@@ -19,14 +19,14 @@ class PushTokenSyncer {
         byte[] payload = ("{\n" +
                 "\t\"header\": {\n" +
                 "\t\t\"schemaRef\": {\n" +
-                "\t\t  \"id\": \"https://ns.adobe.com/acopprod3/schemas/393fe4b3364b0856c909a6476260d45f10b360b058e93caa\",\n" +
+                "\t\t  \"id\": \"https://ns.adobe.com/acopprod3/schemas/393fe4b3364b0856c909a6476260d45f10b360b058e93caa\",\n" + // Messaging SDK Schema in Stage Sandbox for E2E testing
                 "\t\t  \"contentType\": \"application/vnd.adobe.xed-full+json;version=1.28\"\n" +
                 "\t\t},\n" +
                 "\t\t\"imsOrgId\": \"FAF554945B90342F0A495E2C@AdobeOrg\",\n" +
                 "\t\t\"source\": {\n" +
                 "\t\t  \"name\": \"mobile\"\n" +
                 "\t\t},\n" +
-                "\t\t\"datasetId\": \"5ef3e83e6919231915e11ca1\"\n" +
+                "\t\t\"datasetId\": \"5ef3e83e6919231915e11ca1\"\n" + // Messaging SDK dataset in Stage Sandbox for E2E testing
                 "\t\t},\n" +
                 "\t\t\"body\": {\n" +
                 "\t\t\"xdmMeta\": {\n" +
