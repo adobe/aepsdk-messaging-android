8100dfee6ea49774bbca764b386c95070fa66089
ravinsingh jain <ravjain@adobe.com>
2020-12-01 11:06:55 -0800

    Updating the messaging tests with 2 new apis

--- code/messaging/src/test/java/com/adobe/marketing/mobile/MessagingTests.java
+++ code/messaging/src/test/java/com/adobe/marketing/mobile/MessagingTests.java
@@ -10,20 +10,41 @@
 */
 package com.adobe.marketing.mobile;
 
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.adobe.marketing.mobile.MessagingConstant.EventDataKeys.Messaging.TRACK_INFO_KEY_ACTION_ID;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({MobileCore.class})
+@PrepareForTest({MobileCore.class, Intent.class})
 public class MessagingTests {
+    @Mock
+    Intent mockIntent;
+
     @Before
     public void before() {
         PowerMockito.mockStatic(MobileCore.class);
@@ -63,4 +84,80 @@ public class MessagingTests {
         extensionErrorCallback.error(ExtensionError.UNEXPECTED_ERROR);
     }
 
+    // ========================================================================================
+    // addPushTrackingDetails
+    // ========================================================================================
+    @Test
+    public void test_addPushTrackingDetails_WhenParamsAreNull() {
+        // test
+        boolean done = Messaging.addPushTrackingDetails(null, null, null);
+
+        // verify
+        Assert.assertFalse(done);
+    }
+
+    @Test
+    public void test_addPushTrackingDetails() {
+        String mockMessageId = "mockMessageId";
+        String mockXDMData = "mockXDMData";
+        Map<String, String> mockDataMap = new HashMap<>();
+        mockDataMap.put(MessagingConstant.TrackingKeys._XDM, mockXDMData);
+
+        // test
+        boolean done = Messaging.addPushTrackingDetails(mockIntent, mockMessageId, mockDataMap);
+
+        // verify
+        Assert.assertTrue(done);
+        verify(mockIntent, times(1)).putExtra(MessagingConstant.EventDataKeys.Messaging.TRACK_INFO_KEY_MESSAGE_ID, mockMessageId);
+        verify(mockIntent, times(1)).putExtra(MessagingConstant.EventDataKeys.Messaging.TRACK_INFO_KEY_ADOBE_XDM, mockXDMData);
+    }
+
+    // ========================================================================================
+    // handleNotificationResponse
+    // ========================================================================================
+    @Test
+    public void test_handleNotificationResponse_WhenParamsAreNull() {
+        // test
+        Messaging.handleNotificationResponse(null, false, null);
+
+        // verify
+        PowerMockito.verifyStatic(MobileCore.class, Mockito.times(0));
+        MobileCore.dispatchEvent(any(Event.class), any(ExtensionErrorCallback.class));
+    }
+
+    @Test
+    public void test_handleNotificationResponse() {
+        final ArgumentCaptor<Event> eventCaptor = ArgumentCaptor.forClass(Event.class);
+        String mockActionId = "mockActionId";
+        String mockXdm = "mockXdm";
+
+        try {
+            PowerMockito.whenNew(Intent.class)
+                    .withNoArguments().thenReturn(mockIntent);
+        } catch (Exception e) {
+            com.adobe.marketing.mobile.Log.debug("MessagingTest", "Intent exception");
+        }
+
+        when(mockIntent.getStringExtra(anyString())).thenReturn(mockXdm);
+
+        // test
+        Messaging.handleNotificationResponse(mockIntent, true, mockActionId);
+
+        // verify
+        verify(mockIntent, times(2)).getStringExtra(anyString());
+
+        PowerMockito.verifyStatic(MobileCore.class, Mockito.times(1));
+        MobileCore.dispatchEvent(eventCaptor.capture(), any(ExtensionErrorCallback.class));
+
+        // verify event
+        Event event = eventCaptor.getValue();
+        EventData eventData = event.getData();
+        assertNotNull(eventData);
+        assertEquals(MessagingConstant.EventType.MESSAGING.toLowerCase(), event.getEventType().getName());
+        try {
+            assertEquals(eventData.getString2(TRACK_INFO_KEY_ACTION_ID), mockActionId);
+        } catch (VariantException e) {
+            com.adobe.marketing.mobile.Log.debug("MessagingTest", "getString2 variant exception, error : %s", e.getMessage());
+        }
+    }
 }
