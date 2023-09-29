567c4b9cbcfb1f93abb17a59714431a0e0a91c39
Ryan Morales <rymorale@adobe.com>
2023-09-29 10:10:38 -0700

    set version to 2.3.0-cbe-beta

--- code/messaging/src/phone/java/com/adobe/marketing/mobile/Messaging.java
+++ code/messaging/src/phone/java/com/adobe/marketing/mobile/Messaging.java
@@ -33,7 +33,7 @@ import java.util.List;
 import java.util.Map;
 
 public final class Messaging {
-    private static final String EXTENSION_VERSION = "2.2.0";
+    private static final String EXTENSION_VERSION = "2.3.0-cbe-beta";
     private static final String LOG_TAG = "Messaging";
     private static final String CLASS_NAME = "Messaging";
 
