37e6c824bd92da1cd574ebb099e9ba98f72904e5
Spoorthi Pujari <spujari@adobe.com>
2024-10-21 14:10:56 -0700

    refactor package structure

--- code/messaging/src/main/java/com/adobe/marketing/mobile/aepcomposeui/uimodels/AepDismissButton.kt
+++ code/messaging/src/main/java/com/adobe/marketing/mobile/aepcomposeui/uimodels/AepDismissButton.kt
@@ -9,7 +9,7 @@
   governing permissions and limitations under the License.
 */
 
-package com.adobe.marketing.mobile.aepuitemplates.uimodels
+package com.adobe.marketing.mobile.aepcomposeui.uimodels
 
 /**
  * Represents a dismiss button for a content card in the small image template.
