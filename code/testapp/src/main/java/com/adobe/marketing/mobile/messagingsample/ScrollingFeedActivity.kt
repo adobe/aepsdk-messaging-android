37884f1cd94a78e443b61865343aa8e2993f2822
Spoorthi Pujari <spujari@adobe.com>
2024-11-01 11:38:13 -0700

    change title and body style field to be consistent with rest

--- code/testapp/src/main/java/com/adobe/marketing/mobile/messagingsample/ScrollingFeedActivity.kt
+++ code/testapp/src/main/java/com/adobe/marketing/mobile/messagingsample/ScrollingFeedActivity.kt
@@ -169,7 +169,7 @@ class ScrollingFeedActivity : AppCompatActivity() {
 
         // Create row with composables from AepUI instances
         LazyRow {
-            items(reorderedAepUIList) { aepUI ->
+            items(aepUiList) { aepUI ->
                 when (aepUI) {
                     is SmallImageUI -> {
                         val state = aepUI.getState()
