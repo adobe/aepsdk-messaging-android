abe464309e56162ce1baab1d00bc7c472f5590ea
Spoorthi Pujari <spujari@adobe.com>
2025-07-22 16:59:06 -0700

    Migrate to CentralSonatype publishing

--- code/settings.gradle.kts
+++ code/settings.gradle.kts
@@ -24,7 +24,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         mavenLocal()
-        maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
+        maven { url = uri("https://central.sonatype.com/repository/maven-snapshots/") }
         maven { url = uri("https://jitpack.io") }
     }
 }
