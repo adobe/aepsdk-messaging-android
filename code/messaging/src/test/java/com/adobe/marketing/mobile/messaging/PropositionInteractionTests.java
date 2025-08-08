f2ed87d6415d9e2ce623a803c8d6776722371565
Kevin Lind <40409666+kevinlind@users.noreply.github.com>
2025-08-07 21:44:37 -0700

    [feature] push token sync optimization (#380)
    * handle reset identities event by removing persisted push token
    
    * add unit tests for reset identities handling
    
    * add reset identities and reset complete event handling
    
    - reset complete event handling is not currently working as the reset complete edge identity event is being dispatched as a paired event to the generic identity reset identities event. the reset complete event will need to be sent as an unpaired event to be listened for in the messaging extension.
    
    * remove resetComplete event handling
    
    the app developer will be responsible for resyncing the push token with newly generated ecid after resetIdentities is called
    
    * remove unused function
    
    * review updates
    
    * update the push token sync configuration flag and should sync logic
    
    * add code comment
    
    * review updates
    
    * fix format error
    
    * batch content card proposition triggers
    
    * fix log
    
    * Adding back code after bad merge cut it out.
    
    * Fix test app after merge
    
    ---------
    
    Co-authored-by: Ryan Morales <rymorale@adobe.com>

--- code/messaging/src/test/java/com/adobe/marketing/mobile/messaging/PropositionInteractionTests.java
+++ code/messaging/src/test/java/com/adobe/marketing/mobile/messaging/PropositionInteractionTests.java
@@ -261,4 +261,95 @@ public class PropositionInteractionTests {
                         MessagingTestConstants.EventDataKeys.Messaging.Inbound.Key
                                 .PROPOSITION_ACTION));
     }
+
+    @Test
+    public void test_getPropositionInteractionXDM_returnsValidMapWhenEventTypeIsSuppressDisplay() {
+        // setup
+        String mockItemId = "mockItemId";
+        String mockInteraction = "mockInteraction";
+        PropositionInteraction messagingPropositionInteraction =
+                new PropositionInteraction(
+                        MessagingEdgeEventType.SUPPRESS_DISPLAY,
+                        mockInteraction,
+                        mockPropositionInfo,
+                        mockItemId,
+                        null);
+
+        // test
+        Map<String, Object> xdm = messagingPropositionInteraction.getPropositionInteractionXDM();
+
+        // verify
+        assertNotNull(xdm);
+        assertFalse(xdm.isEmpty());
+        assertEquals(
+                MessagingEdgeEventType.SUPPRESS_DISPLAY.toString(),
+                xdm.get(MessagingTestConstants.EventDataKeys.Messaging.XDMDataKeys.EVENT_TYPE));
+        Map<String, Object> experience =
+                (Map<String, Object>) xdm.get(MessagingTestConstants.TrackingKeys.EXPERIENCE);
+        assertNotNull(experience);
+
+        Map<String, Object> decisioning =
+                (Map<String, Object>)
+                        experience.get(
+                                MessagingTestConstants.EventDataKeys.Messaging.Inbound.Key
+                                        .DECISIONING);
+        assertNotNull(decisioning);
+
+        Map<String, Object> propositionEventType =
+                (Map<String, Object>)
+                        decisioning.get(
+                                MessagingTestConstants.EventDataKeys.Messaging.Inbound.Key
+                                        .PROPOSITION_EVENT_TYPE);
+        assertNotNull(propositionEventType);
+
+        List<Map<String, Object>> propositions =
+                (List<Map<String, Object>>)
+                        decisioning.get(
+                                MessagingTestConstants.EventDataKeys.Messaging.Inbound.Key
+                                        .PROPOSITIONS);
+        assertNotNull(propositions);
+        assertEquals(1, propositions.size());
+        assertEquals(
+                mockPropositionInfo.id,
+                propositions
+                        .get(0)
+                        .get(MessagingTestConstants.EventDataKeys.Messaging.Inbound.Key.ID));
+        assertEquals(
+                mockPropositionInfo.scope,
+                propositions
+                        .get(0)
+                        .get(MessagingTestConstants.EventDataKeys.Messaging.Inbound.Key.SCOPE));
+        assertEquals(
+                mockPropositionInfo.scopeDetails,
+                propositions
+                        .get(0)
+                        .get(
+                                MessagingTestConstants.EventDataKeys.Messaging.Inbound.Key
+                                        .SCOPE_DETAILS));
+
+        List<Map<String, Object>> items =
+                (List<Map<String, Object>>)
+                        propositions
+                                .get(0)
+                                .get(
+                                        MessagingTestConstants.EventDataKeys.Messaging.Inbound.Key
+                                                .ITEMS);
+        assertNotNull(items);
+        assertEquals(1, items.size());
+        assertEquals(
+                mockItemId,
+                items.get(0).get(MessagingTestConstants.EventDataKeys.Messaging.Inbound.Key.ID));
+
+        Map<String, Object> propositionAction =
+                (Map<String, Object>)
+                        decisioning.get(
+                                MessagingTestConstants.EventDataKeys.Messaging.Inbound.Key
+                                        .PROPOSITION_ACTION);
+        assertNotNull(propositionAction);
+        assertEquals(1, propositionAction.size());
+        assertEquals(
+                mockInteraction,
+                propositionAction.get(
+                        MessagingTestConstants.EventDataKeys.Messaging.Inbound.Key.REASON));
+    }
 }
