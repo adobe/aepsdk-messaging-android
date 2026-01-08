b7863f35715af978481b6f91740e3c29155e1e17
Navratan Soni <navratans@adobe.com>
2026-01-08 10:48:21 +0530

    Code for exd support

--- code/messaging/src/main/java/com/adobe/marketing/mobile/messaging/MessagingConstants.java
+++ code/messaging/src/main/java/com/adobe/marketing/mobile/messaging/MessagingConstants.java
@@ -143,6 +143,10 @@ public final class MessagingConstants {
         static final String LAUNCHES_VALUE = "value";
         static final String DATASET_ID = "datasetId";
         static final String COLLECT = "collect";
+        static final String EXD_REQUEST_ID = "exdRequestID";
+        static final String OPEN = "open";
+        static final String INTERACT = "interact";
+        static final String DISMISS = "dismiss";
 
         private TrackingKeys() {}
     }
