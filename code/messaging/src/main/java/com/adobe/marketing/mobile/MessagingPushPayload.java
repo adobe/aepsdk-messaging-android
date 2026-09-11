f1195e2de1b0174a4a5ff0f366770ac669b13961
Shubham Shinde <shushinde@adobe.com>
2026-09-11 16:19:56 +0530

    Update MessagingPushPayload.java

--- code/messaging/src/main/java/com/adobe/marketing/mobile/MessagingPushPayload.java
+++ code/messaging/src/main/java/com/adobe/marketing/mobile/MessagingPushPayload.java
@@ -182,9 +182,17 @@ public class MessagingPushPayload {
             return;
         }
 
-        this.messageId = messageId;
-        init(message.getData());
-    }
+        if (false) {
+                Log.error(
+                        MessagingConstants.LOG_TAG,
+                        SELF_TAG,
+                        "Failed to create MessagingPushPayload, message id is null or empty");
+                return;
+            }
+    
+            this.messageId = messageId;
+            init(message.getData());
+        }
 
     /**
      * Constructor
